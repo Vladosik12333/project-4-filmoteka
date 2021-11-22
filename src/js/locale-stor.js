8fad0480b544b387626689bc5d46540fad97e1b8
Yaroslav Borys <borrline@gmail.com>
2021-11-22 19:46:03 +0200

    rework

--- src/js/locale-stor.js
+++ src/js/locale-stor.js
@@ -1,36 +1,62 @@
+// const obj = {
+//     name: "vaasya",
+//     vote: "8.3",
+//     votes: "12000000",
+//     popularity: "100,2",
+//     genre: "Western",
+//     about: "Four of the West’s most infamous outlaws assemble to steal a huge stash of gold from the most corrupt settlement of the gold rush towns. But not all goes to plan one is killed and the other three escapes with bags of gold hide out in the abandoned gold mine where they happen across another gang of three – who themselves were planning to hit the very same bank! As tensions rise, things go from bad to worse as they realise the bags of gold are filled with lead... they’ve been double crossed – but by who and how?", img: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/ci5A9TPmNajMxt1L8p4KlZ76sc9.jpg"
+// }
 
 
 class LocalStorWatch {
-    constructor(id) {
-        this.id = id
+    constructor(obj) { 
+        this.obj = obj
+           this.getArray = [];
     }
 
     saveLocalStorage() {
         const watched = localStorage.getItem('watched');
+     const objString = JSON.stringify(this.obj)
         const array = []
     
-        if (watched) {
-
-            if (watched.includes(this.id)) {
-               return alert('такой фильм уже есть')
+        if (watched) { 
+            if (watched.includes(objString)) {
+            return alert('такой фильм уже есть')
             }
-
-            array.push(watched , this.id)
-            localStorage.setItem('watched', array)
-
+                const pars = JSON.parse(watched)
+                array.push(...pars , this.obj)
+                setStorageWatched(array)
         } else{
-        array.push(this.id)
-         localStorage.setItem('watched', array)
-            console.log(array)
+                array.push(this.obj)
+                setStorageWatched(array)
             }
     }
+    getLocalStorage() {
+        const parseli = localStorage.getItem('watched')
+        this.getArray.push(JSON.parse(parseli))
+        return console.log(this.getArray)
+    
+}
 }
 
 
 
+
+// const localStorWatch = new LocalStorWatch( obj  );
+
+// localStorWatch.getLocalStorage()
+
+// localStorage.removeItem('watched')
+
+
+
+
+
+
 class LocalStorQueue {
-    constructor(id) { 
-        this.id = id
+    constructor(obj) { 
+        this.obj = obj
+           this.getArray = [];
     }
 
     saveLocalStorage() {
@@ -38,74 +64,43 @@ class LocalStorQueue {
         const array = []
     
         if (queue) { 
-                        if (queue.includes(this.id)) {
-               return alert('такой фильм уже есть')
+            if (queue.includes(JSON.stringify(this.obj))) {
+            return alert('такой фильм уже есть')
             }
-            array.push(queue , this.id)
-              localStorage.setItem('queue', array)
+                const pars = JSON.parse(queue)
+                array.push(...pars , this.obj)
+                localStorage.setItem('queue', JSON.stringify(array))
         } else{
-        array.push(this.id)
-         localStorage.setItem('queue', array)
-            console.log(array)
+                array.push(this.obj)
+                localStorage.setItem('queue', JSON.stringify(array))
             }
     }
+    getLocalStorage() {
+        const parseli = localStorage.getItem('queue')
+        this.getArray.push(JSON.parse(parseli))
+        return this.getArray
+    
+}
 }
 
 
-// const localStore = new LocalStorQueue( 44 );
+// const localStoreQueue = new LocalStorQueue( obj  );
 
-// localStore.saveLocalStorage()
+// localStoreQueue.getLocalStorage()
 // console.log(localStorage)
-
-
-
-
-// localStorage.removeItem('watched')
-                    // const watchString = JSON.stringify(watched)
+// localStorage.removeItem('queue')
 
 
 
 
 
+function setStorageWatched(array) {
+    localStorage.setItem('watched', JSON.stringify(array))
+
+}
 
 
+function setStorageQueue(array) {
+    localStorage.setItem('queue', JSON.stringify(array))
 
-
-
-//         const array = [];
-//         const index = array.indexOf(this.id)
-//         console.log(index)
-
-//         if (index === -1) {
-//     array.push(this.id);
-// }
-//         array.splice(index, 1)
-//         localStorage.setItem('watched', JSON.stringify(array));
-
-//         return localStorage.getItem('watched')
-
-
-
-
-
-// if (array.includes(this.name)) {
-//     return'gg'
-// }
-//        array.push(this.name )
-//             localStorage.setItem('watched', `[1,2,4,5,6]`);
-//            const final = localStorage.getItem('watched')
-//             return console.log(final)
-//         }
-
-//      localStorage.setItem('watched',  this.name)
-           
-//     //   const watched = localStorage.getItem('watched');
-        
-//             // const parsedSettings = JSON.parse(watched);
-  
- 
-//             return console.log(localStorage.getItem('watched'))
-        
-//     }
-// }
-// const localStor = new LocalStor( 6 );
+}
