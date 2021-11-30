ff8a8e631f78a912af7049ec5c6ea9e196cb91ca
Yaroslav Borys <borrline@gmail.com>
2021-11-30 22:18:20 +0200

    feature/deleteFilm/fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,8 @@
+
 import template from '../templates/card-modal.hbs';
 import ApiService from './api';
 import LocalStorage from './locale-stor';
 import renderLocalStorageExport from './library'
-import functionRenderFilms from './render';
-
-
-
 
 const refs = {
   backdrop: document.querySelector('.js-backdrop-film'),
@@ -14,15 +11,13 @@ const refs = {
   closeModalButton: document.querySelector('.js-modal-button-close'),
   bodyEl: document.body,
   cardContainer: document.querySelector('.gallery'),
-headers: document.querySelector('.header')
+  headers: document.querySelector('.header')
 };
 
 const api = new ApiService();
 const ls = new LocalStorage();
 
 refs.cardContainer.addEventListener('click', openModalFilm);
-
-
 
 function openModalFilm(evt) {
   evt.preventDefault();
@@ -32,52 +27,42 @@ function openModalFilm(evt) {
     return;
   }
 
-
   api.fetchMoviesById(isFilmCardId).then(response => {
     refs.modalFilmInfo.innerHTML = template(response);
 
     const modalWatchedBtn = document.querySelector('#modal-watched');
     const modalQueueBtn = document.querySelector('#modal-queue');
-    const libraryWatchedBtn = document.querySelector('#library-watched');
-   const libraryQueueBtn = document.querySelector('#library-queue');
+    const libraryBtnWatched = document.querySelector('#library-watched');
+    const libraryBtnQueue = document.querySelector('#library-queue');
 
-
-    changeClassButt (response,'watched',modalWatchedBtn );
-    changeClassButt (response,'queue',modalQueueBtn );
+    ls.changeClassButt(response, 'watched', modalWatchedBtn);
+    ls.changeClassButt(response, 'queue', modalQueueBtn);
 
     modalWatchedBtn.addEventListener('click', (evt) => {
       evt.target.blur()
-    if(ls.searchDoubledId(response , 'watched')){
-      addClass(evt.currentTarget, 'watched')
-       ls.saveWatched(response);
-    }else{ 
-      deleteClass(evt.currentTarget, 'watched')
-      ls.deleteWatch(response)
-    }
-    if (!refs.headers.classList.contains('header--home')){
-      renderLocalStorageExport('watched');
-      libraryWatchedBtn.classList.add('button--is-active');
-      libraryQueueBtn.classList.remove('button--is-active');
-    }
-
-  //  evt.preventDefault()
-
+      if (ls.searchDoubledId(response , 'watched')){
+        ls.saveWatched(response, modalWatchedBtn)
+      }else{
+        ls.deleteWatch(response, modalWatchedBtn);
+      }
+      if (!refs.headers.classList.contains('header--home')){
+        if(libraryBtnWatched.classList.contains('button--is-active')){ 
+          renderLocalStorageExport('watched');
+        }
+      }
     });
     modalQueueBtn.addEventListener('click', (evt) => {
       evt.target.blur()
       if (ls.searchDoubledId(response , 'queue')){
-        addClass(modalQueueBtn, 'queue')
-     ls.saveQueue(response);
-    }else{ 
-      deleteClass(modalQueueBtn, 'queue') 
-      ls.deleteQueue(response)
-
-    }
-    if (!refs.headers.classList.contains('header--home')){
-      renderLocalStorageExport('queue');
-      libraryQueueBtn.classList.add('button--is-active');
-      libraryWatchedBtn.classList.remove('button--is-active');
-    }
+        ls.saveQueue(response, modalQueueBtn)
+      }else{
+        ls.deleteQueue(response, modalQueueBtn);
+      }
+      if (!refs.headers.classList.contains('header--home')){
+        if(libraryBtnQueue.classList.contains('button--is-active')){ 
+          renderLocalStorageExport('queue');
+        }        
+      }
     });
   });
 
@@ -108,25 +93,3 @@ function closeModal() {
   refs.backdrop.removeEventListener('click', backdropClick);
   window.removeEventListener('keydown', onPressEsc);
 }
-
-
-
-function changeClassButt (obj, key , butt){
-  if (ls.searchDoubledId(obj , key)) {
-    deleteClass(butt, key);
-  }else{
-    addClass(butt, key);
-  }
-}
-
-
-function addClass (butt, name){
-  butt.textContent = `Delete ${name}`;
-  butt.classList.add('button--is-active');
-}
-function deleteClass(butt , name){
-  butt.classList.remove('button--is-active');
-  butt.textContent = `Add to ${name}`;
-}
-
-
